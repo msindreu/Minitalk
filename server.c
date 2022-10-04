659a5e4e5858e7cb546edb480d6703720030e689
msindreu <msindreuriba@gmail.com>
2022-10-04 18:28:12 +0200

    proves

---
+++ server.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msindreu <msindreu@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 18:19:39 by msindreu          #+#    #+#             */
+/*   Updated: 2022/10/04 18:27:58 by msindreu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+int	main()
+{
+	int	pid;
+	struct	sigaction signal;
+	pid = getpid();
+	printf("PID: %d\n", pid);
+	signal.sa_sigaction = ft_handle;
+	sigfillset(&signal.sa_mask);
+	signal.sa_flags = SA_RESTART;
+	sigaction(SIGUSR1, &signal, NULL);
+	sigaction(SIGUSR2, &signal, NULL);
+	while (1)
+		pause();
+	return (0);
+}

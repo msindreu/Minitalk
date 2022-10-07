e700648442db93032893ce84bac94e1d0bfeaf9d
msindreu <msindreuriba@gmail.com>
2022-10-07 17:53:58 +0200

    no funciona

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: msindreu <msindreu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 18:19:39 by msindreu          #+#    #+#             */
-/*   Updated: 2022/10/04 18:47:49 by msindreu         ###   ########.fr       */
+/*   Updated: 2022/10/07 17:53:26 by msindreu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 #include <signal.h>
 #include <stdio.h>
 
-static void	ft_handle(int	sig, siginfo_t *info, void *contect)
+void	ft_handle(int	sig)//, siginfo_t *info, void *context)
 {
+	static pid_t 
 
 
-
-
+	(void)context;
+	(void)sig;
+	if (sig == SIGUSR1)
+		printf("Senyal 1 rebuda");
+	if (sig == SIGUSR2)
+		printf("Senyal2 rebuda");
 }
-
-
-
 
 int	main()
 {
@@ -32,8 +34,10 @@ int	main()
 	pid = getpid();
 	printf("PID: %d\n", pid);
 	signal.sa_sigaction = ft_handle;
-	sigfillset(&signal.sa_mask);
-	signal.sa_flags = SA_RESTART;
+	//signal.sa_handler = ft_handle;
+	sigemptyset(sigset_t *set);
+//	sigfillset(&signal.sa_mask);
+	signal.sa_flags = 0;
 	sigaction(SIGUSR1, &signal, NULL);
 	sigaction(SIGUSR2, &signal, NULL);
 	while (1)

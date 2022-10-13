75dd78c3c7a40eefe25eedd876fced9fe35e107e
msindreu <msindreuriba@gmail.com>
2022-10-13 16:45:20 +0200

    prova

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: msindreu <msindreu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:31:24 by msindreu          #+#    #+#             */
-/*   Updated: 2022/10/11 12:07:11 by msindreu         ###   ########.fr       */
+/*   Updated: 2022/10/13 16:44:47 by msindreu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,17 @@
 
 int	main(int argc, char **argv)
 {
-int	server_pid;
+	int	server_pid;
 
-server_pid = ft_atoi(argv[1]);
+	if (argc == 3)
+	{
+		server_pid = ft_atoi(argv[1]);
+	}
+	while (argv[2][i] != '\0')
+	{
+		ft_get_and_send
+	}
+
 
 
 

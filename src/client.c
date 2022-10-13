f2670816dbe8505fb026c4004e1a36ad07257f57
msindreu <msindreuriba@gmail.com>
2022-10-13 17:07:37 +0200

    \0

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: msindreu <msindreu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:31:24 by msindreu          #+#    #+#             */
-/*   Updated: 2022/10/13 16:44:47 by msindreu         ###   ########.fr       */
+/*   Updated: 2022/10/13 17:07:12 by msindreu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,36 @@
 #include <signal.h>
 #include <stdio.h>
 
+int	ft_get_and_send_bits(char c, int pid)
+{
+	int		i;
+
+
+
+
+
+
+
+
+}
+
+
 int	main(int argc, char **argv)
 {
 	int	server_pid;
+	int	i;
 
 	if (argc == 3)
 	{
 		server_pid = ft_atoi(argv[1]);
+		i = 0;
+		while (argv[2][i] != '\0')
+		{
+			ft_get_and_send_bits(argv[2][i], server_pid);
+			i++;
+		}
+		ft_get_and_send_bits('\0', server_pid);
 	}
-	while (argv[2][i] != '\0')
-	{
-		ft_get_and_send
-	}
-
-
-
 
 
 
